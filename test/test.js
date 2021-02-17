40833a16eb009850e2a2a51a7fe49e8164c6dc45
abraham-jim <addisauto@gmail.com>
2021-02-16 19:08:46 -0800

    EST-Group- 1 final commit

---
+++ test/test.js
@@ -0,0 +1,34 @@
+var assert = require('assert');
+
+//Testing addTask
+
+describe('taskManager', () => {
+  describe('addTask', () => {
+       
+    it('should be able to add and complete taskManager', () => {
+       
+        assert.ok(['group1', 'task1', 'group1', 02/16/2021, 'status'].indexOf())
+    });
+});
+});
+
+
+//Testing deleteTask
+
+describe('taskManager', () => {
+    describe('deleteTask', () => {
+        it('Should be able to delete the task', () => {
+            assert.ok(['group1', 'task1', 'group1', 02/16/2021, 'status'].indexOf())
+        });
+    });
+});
+
+//Testing getTaskById
+
+describe('taskManager', () => {
+    describe('getTaskById', () => {
+        it('Should be able to get task by ID', () => {
+            assert.ok(['group1', 'task1', 'group1', 02/16/2021, 'status'].indexOf())
+        });
+    });
+});
